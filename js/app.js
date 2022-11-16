280a3bd02dde8a2245d318ea8f16d861be052e68
rk <rebeccakruk@outlook.com>
2022-11-15 20:16:11 -0700

    introducing cookies

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
+function handleButtonClick(event){
+    Cookies.set(`cupcakeConsumed` , `true`);
+}
+
+function login(){
+    Cookies.set(`username`, `bestUser`);
+}
+
+function logout(){
+    Cookies.remove(`username`);
+}
+let username = Cookies.get(`username`);
+document.getElementById(`loginButton`).addEventListener(`click`, login);
+document.getElementById(`logoutButton`).addEventListener(`click`, logout);
+if(username !=undefined){
+    document.getElementById(`userGreeting`).innerText = `Hello ${username}`;
+}
+
+document.getElementById(`cupcakeButton`).addEventListener(`click`, handleButtonClick);
+
+
 function addH3() {
     let header = document.getElementById(`header`);
     header.insertAdjacentHTML(`afterend`, `<h3>Secret hover!</h3>`);
@@ -41,7 +62,7 @@ const muffinLink = `https://theviewfromgreatisland.com/wp-content/uploads/2021/0
 
 let heading = document.getElementById(`header`);
 heading.addEventListener(`mouseover`, addH3);
-heading.addEventListener(`mouseleave`, removeH3);
+// heading.addEventListener(`mouseleave`, removeH3);
 document.body.addEventListener(`keydown`, makeBlue);
 document.body.addEventListener(`keyUp`, makeBisque);
 
